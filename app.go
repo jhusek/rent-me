b8cb2d31da481ea63907cce4ea39f0d4f1077d5a
Husek, Jakub <jakub.husek@intel.com>
2026-05-03 21:55:46 -0700

    chore: bump version to 1.0.0, fix workflow go version and release notes

--- app.go
+++ app.go
@@ -17,7 +17,7 @@ import (
 	wailsruntime "github.com/wailsapp/wails/v2/pkg/runtime"
 )
 
-const AppVersion = "0.1.0"
+const AppVersion = "1.0.0"
 const githubRepo = "jhusek/rent-me"
 
 type App struct {
